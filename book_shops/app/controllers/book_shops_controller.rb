1ebd1a5c4352151f2349a8c9bdc1ca5e4391eeca
siwakotisurya <siwakotisurya@outlook.com>
2015-10-06 22:39:23 +0545

    finalize template for rails

--- book_shops/app/controllers/book_shops_controller.rb
+++ book_shops/app/controllers/book_shops_controller.rb
@@ -1,14 +1,24 @@
 class BookShopsController < ApplicationController
+  
+  layout "book_shops"
   def index
+    @bookshops = BookShop.all
   end
 
   def show
   end
 
   def new
+    @shop = BookShop.new
   end
 
   def create
+    @shop = BookShop.new(set_database_field)
+    if @shop.save
+      render "new"
+    else
+       render "new"
+    end
   end
 
   def edit
@@ -21,5 +31,9 @@ class BookShopsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private def set_database_field
+  params.require(:book_shop).permit(:shop_name, :shop_location, :phone_number, :email_address)
   end
 end
--- book_shops/app/models/book_shop.rb
+++ book_shops/app/models/book_shop.rb
@@ -1,2 +1,3 @@
 class BookShop < ActiveRecord::Base
+	validates :shop_name,:shop_location, :phone_number, :email_address, :presence=>true
 end
